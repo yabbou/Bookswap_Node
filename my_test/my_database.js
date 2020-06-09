100c38998cbfb43d176790ff6760db214376482e
Yaakov Abbou <jacobabbou@outlook.com>
2020-06-09 17:49:48 -0400

    Temporarily removed select * from book

--- my_test/my_database.js
+++ my_test/my_database.js
@@ -17,49 +17,53 @@ con.connect(function (err) {
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     res.writeHead(200, { 'Content-type': 'text/html' });
-    fs.readFile("./books.html", function (err, data) {
+
+    fs.readFile("./sell_book.html", function (err, data) {
         if (err) {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             return res.end("404 Not Found");
         }
+
         res.write(data);
-        if (q.pathname === "/books") {
+        if (q.pathname === "/sell") {
             var query = q.query;
-            res.write("Name= " + query.fname + " " + query.lname);
-            res.write("<br>Username= " + query.uname);
-            res.write("<br>Password= " + query.pass);
-            res.write("<br>Email= " + query.email);
-            res.write("<br>Profession= " + query.job);
-            res.write("<br>Flip Out Type= " + query.fo_type + "<br><br>");
-            if (query.pass === query.pass_conf && query.email === query.email_conf) {
-                var sql2 = "INSERT INTO users(id, username, password, email, first_name,"
-                    + " last_name, profession, fo_type, date_joined, authorization)"
-                    + " VALUES (NULL, \'" + query.uname + "\', \'" + query.pass +
-                    "\', \'" + query.email + "\', \'" + query.fname + "\', \'" +
-                    query.lname + "\', \'" + query.job + "\', \'" + query.fo_type +
-                    "\', CURRENT_TIMESTAMP, 'reg')";
-                con.query(sql2, function (err, result) {
+
+            res.write("Title= " + query.title);
+            res.write("<br>Category= " + query.cat);
+            res.write("<br>ISBN-10= " + query.isbn);
+            res.write("<br>Professor= " + query.prof);
+
+            if (query.pass === query.pass_conf && query.email === query.email_conf) { //??
+                var sqlInsert = "INSERT INTO book (Title, Category, ISBN-10, Professor) VALUES ?";
+                var VALUES = [query.title, query.cat, query.isbn, query.prof];
+
+                con.query(sqlInsert, [VALUES], function (err, result) {
                     if (err) throw err;
-                    console.log("Added to the Database ");
+                    console.log("Added to the Database.");
                 });
             } else {
-                console.log("Passwords or Emails Don't Match");
+                console.log("Unsuccessful upload... please try again.");
             };
         };
     });
-    var sql = "SELECT * FROM users";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Database Shown");
-        
-        if (result) {
-            for (var i of result) {
-                res.write("ID: " + i.id + "<br>Username: " + i.username +
-                    "<br>Password: " + i.password + "<br>Email: " + i.email +
-                    "<br>First Name: " + i.first_name + "<br>Last Name: " + i.last_name +
-                    "<br>Profession: " + i.profession + "<br>Flip Out Type: " +
-                    i.fo_type + "<br><br>");
-            };
-        };
-    });
+
+    // var sql = "SELECT * FROM book";
+    // con.query(sql, function (err, result) {
+    //     if (err) throw err;
+    //     console.log("Database Shown");
+
+    //     if (result) {
+    //         console.log(result);
+    //         for (var book of result) {
+    //             console.log(book);
+
+    //             book.forEach(element => {
+    //                 res.write(element);
+    //             });
+    //         }
+    //     }
+    // }
+   
+    
+
 }).listen(3000);
