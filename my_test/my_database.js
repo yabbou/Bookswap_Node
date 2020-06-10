de68aa1f1313b953131f6ed872b054657a345b52
Yaakov Abbou <jacobabbou@outlook.com>
2020-06-09 20:27:21 -0400

    Completed hw 11

--- my_test/my_database.js
+++ my_test/my_database.js
@@ -28,11 +28,6 @@ http.createServer(function (req, res) {
         if (q.pathname === "/sell") {
             var query = q.query;
 
-            res.write("Title= " + query.title);
-            res.write("<br>Category= " + query.cat);
-            res.write("<br>ISBN-10= " + query.isbn);
-            res.write("<br>Professor= " + query.prof);
-
             if (query.pass === query.pass_conf && query.email === query.email_conf) {
                 var sqlInsert = "INSERT INTO book (Title, Category, `ISBN_10`,`ISBN-13`, Professor) VALUES (?)";
                 var VALUES = [query.title, query.cat, query.isbn, 0000000000000, query.prof];
@@ -40,33 +35,36 @@ http.createServer(function (req, res) {
                 con.query(sqlInsert, [VALUES], function (err, result) {
                     if (err) throw err;
                     console.log("Added to the Database.");
+                    displayBooks(res);
                 });
             } else {
                 console.log("Unsuccessful upload... please try again.");
             };
         };
     });
+    
+    displayBooks(res);
 
+}).listen(3000);
+
+function displayBooks(res) {
     var sql = "SELECT * FROM book";
     con.query(sql, function (err, result) {
-        if (err) throw err;
+        if (err)
+            throw err;
         console.log("Database Shown");
-
         if (result) {
             res.write("<table border=1><tr><th>Title</th><th>Major</th><th>ISBN-10</th><th>Prof</th></tr >");
             for (var book of result) {
-                res.write(
-                    "<tr>" +
+                res.write("<tr>" +
                     "<td>" + book.Title + "</td>" +
                     "<td>" + book.Category + "</td>" +
                     "<td>" + book.ISBN_10 + "</td>" +
-                    "<td>" + book.Professor + "</td>"+
-                    "</tr>"
-                );
+                    "<td>" + book.Professor + "</td>" +
+                    "</tr>");
             }
             res.write("</table>");
-
         }
     });
+}
 
-}).listen(3000);
